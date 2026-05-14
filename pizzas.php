d4954a334b94ccb7aed463466814305ae73e771b
NICHOLLAS AUGUSTO PEREIRA SAMPAIO <nichollas.apsampaio@senacsp.edu.br>
2026-05-14 19:16:27 -0300

    VAMOS

--- pizzas.php
+++ pizzas.php
@@ -1,10 +1,20 @@
 <?php
 
     include 'conexao.php';
-    
-   $x = R::dispense('pizzas');
 
-   $x->nome = 'Atum';
-    $x['ingredientes'] = 'Atum,Cebola,queijo,Azeitona'
-     $x->preco = 40.90;
+//    $x = R::dispense('pizzas');
+
+//    $x->nome = 'Atum';
+//     $x['ingredientes'] = 'Atum,Cebola,queijo,Azeitona';
+//      $x->preco = 40.90;
+
+
+
+   R::store($x);
+   $x_edit = R::load('pizzasz',1);
+   $x_edit['preco']= 55.50;
+   $x_edit['ingredientes'] =  'Atum, Cebola, queijo, Azeitona, presunto, fandangos';
+   R::store($x_edit);
+
+
 ?>
